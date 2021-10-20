729d1a42006550f47b7f7b448c4ea95836e38aea
zhoumingcheng <zhoumingcheng@beyondcent.com>
2021-10-20 11:11:17 +0800

    增加jobtemplate crd

--- main.go
+++ main.go
@@ -74,6 +74,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "JobFlow")
 		os.Exit(1)
 	}
+	if err = (&controllers.JobTemplateReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("JobTemplate"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "JobTemplate")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
